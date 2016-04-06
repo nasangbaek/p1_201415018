e4b9c3d570d8707205bf472732a5f6fed147291a
nasangbaek <nasangbaek@naver.com>
2016-04-06 15:03:42 +0900

    6Wmain_multiplesof3_5
    6Wmain_multiplesof3_5

---
+++ 6Wmain_multiplesof3_5.py
@@ -0,0 +1,9 @@
+﻿def sumOfMultiplesOf3_5(begin,end):
+    sum=0
+    for i in range(begin,end):
+        if i%3==0 or i%5==0:
+            sum=sum+i
+    print sum
+    return sum
+
+sumOfMultiplesOf3_5(1,1000)
