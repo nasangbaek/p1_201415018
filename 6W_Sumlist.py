e5548ba24820c63a21439f9dbb7e78cca0bc3ce9
nasangbaek <nasangbaek@naver.com>
2016-04-11 23:55:37 +0900

    4월 11일 sumlist 4로나누어지고 5로 나누어지지 않는 합계
    4월 11일 sumlist 4로나누어지고 5로 나누어지지 않는 합계

---
+++ 6W_Sumlist.py
@@ -0,0 +1,22 @@
+﻿
+def sumlist(alist):
+    x1=list()
+    for i in range(0,alist):
+        if i%4==0 and i%5!=0:
+            x1.append(i)
+    sum=0
+    print x1
+    for x in range(0,len(x1)):
+        sum=sum+x1[x]
+    print sum
+
+def lab6():
+    alist=1000
+    print 'alist=1000, sumlist(alist)'
+    sumlist(alist)
+
+def main():
+	lab6()
+
+if __name__=="__main__":
+	main()
