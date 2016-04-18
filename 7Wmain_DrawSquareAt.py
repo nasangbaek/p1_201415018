21a6022d39d53aafd681c49ce92d9f9003008fa9
nasangbaek <nasangbaek@naver.com>
2016-04-18 22:58:47 +0900

    7Wmain_draw
    7Wmain_draw

---
+++ 7Wmain_DrawSquareAt.py
@@ -0,0 +1,27 @@
+﻿
+def drawSquareAt(size, pos):
+    import turtle
+    wn=turtle.Screen()
+    t1=turtle.Turtle()
+    tracks=list()
+    t1.penup()
+    t1.setpos(pos)
+    t1.pendown()
+    for i in range(0,4):
+        t1.forward(size)
+        t1.left(90)
+        tracks.append(t1.pos())
+    return tracks
+
+drawSquareAt(100,(0,0))
+
+def lab7():
+    pos=(0,0)
+    size=100
+    drawSquareAt(size, pos)
+
+def main():
+    lab7()
+
+if __name__=="__main__":
+	main()
