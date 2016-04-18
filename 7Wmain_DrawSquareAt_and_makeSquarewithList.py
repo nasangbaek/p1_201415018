abe8308dd3f1656d34536ce85f6e5fdc50842014
nasangbaek <nasangbaek@naver.com>
2016-04-19 01:27:39 +0900

    교수님이 강의시간에 하신 내용과 숙제입니다.
    1번은 강의시간에 있는거 올린거고
    2번은 저장된 좌표를 리스트로 저장시켜 다시 사각형을 그리는 내용입니다. 그림으로 보면 모양은 같지만 자세히 보시면 함수의 내용은
    다릅니다.

---
+++ 7Wmain_DrawSquareAt_and_makeSquarewithList.py
@@ -0,0 +1,42 @@
+﻿
+
+def drawSquareAt(size, pos):
+    global x1
+    global sangbaek
+    x1=list()
+    import turtle
+    wn=turtle.Screen()
+    sangbaek=turtle.Turtle()
+    sangbaek.penup()
+    sangbaek.setpos(pos)
+    sangbaek.pendown()
+    for i in range(0,4):
+        sangbaek.forward(size)
+        sangbaek.left(90)
+        x1.append(sangbaek.pos())
+    print x1
+
+drawSquareAt(100,(0,0))
+
+
+def drawSquareAtinList():
+    sangbaek.home()
+    sangbaek.clear()
+    for j in x1:
+        sangbaek.goto(j)
+
+
+def lab7():
+    import turtle
+    wn=turtle.Screen()
+    sangbaek=turtle.Turtle()
+    pos=(0,0)
+    size=100
+    drawSquareAt(size, pos)
+    drawSquareAtinList()
+
+def main():
+    lab7()
+
+if __name__=="__main__":
+	main()
